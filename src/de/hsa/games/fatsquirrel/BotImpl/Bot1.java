b259f9d15842751b98cee43432dcb31aa4ccad51
Biskit1943 <biskit1943@gmx.de>
2016-06-13 12:47:18 +0200

    Nothing

--- src/de/hsa/games/fatsquirrel/BotImpl/Bot1.java
+++ src/de/hsa/games/fatsquirrel/BotImpl/Bot1.java
@@ -1,7 +1,13 @@
 package de.hsa.games.fatsquirrel.BotImpl;
 
+import de.hsa.games.fatsquirrel.Core.Entity.Entities.Squirrel.Bot.MasterSquirrelBot;
+import de.hsa.games.fatsquirrel.Core.Movement.XY;
+
 /**
  * Created by max on 06.06.16 08:50.
  */
-public class Bot1 {
+public class Bot1 extends MasterSquirrelBot{
+    public Bot1(int ID, XY xy) {
+        super(ID, xy);
+    }
 }
